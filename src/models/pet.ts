0ce1055a1b66f755249842c3ddeab94c5cf81e17
Renan Luiz <renanluizz1996@gmail.com>
2023-05-02 06:42:26 -0300

    Criando arquivo do model e criando funções para manipulação dos itens do array

---
+++ src/models/pet.ts
@@ -0,0 +1,153 @@
+type PetType = 'dog' | 'cat' | 'fish';
+type PetSex = 'Masculino' | 'Feminino'
+
+type Pet = {
+    type: PetType,
+    image: string,
+    name: string,
+    color: string,
+    sex: PetSex
+}
+
+const data: Pet[] = [
+    {
+        type: 'dog',
+        image: 'pastor-alemao.jpg',
+        name: 'Pastor-alemão',
+        color: 'Amarelo e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'labrador.jpg',
+        name: 'Labrador-retriever',
+        color: 'Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'zwergspitz.jpg',
+        name: 'Zwergspitz',
+        color: 'Amarelo',
+        sex: 'Feminino'
+    },
+    {
+        type: 'dog',
+        image: 'husky.jpg',
+        name: 'Husky Siberiano',
+        color: 'Branco e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'golden.jpg',
+        name: 'Golden Retriever',
+        color: 'Amarelo',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'poodle.jpg',
+        name: 'Poodle',
+        color: 'Branco',
+        sex: 'Feminino'
+    },
+    {
+        type: 'dog',
+        image: 'bulldog.jpg',
+        name: 'Bulldog',
+        color: 'Branco e Amarelo',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'persa.jpg',
+        name: 'Persa',
+        color: 'Amarelo',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'mainecoon.jpg',
+        name: 'Maine Coon',
+        color: 'Preto e Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'bengal.jpg',
+        name: 'Bengal',
+        color: 'Branco, Preto e Amarelo',
+        sex: 'Feminino'
+    },
+    {
+        type: 'cat',
+        image: 'siames.jpg',
+        name: 'Siamês',
+        color: 'Amarelo e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'sphynx.jpg',
+        name: 'Sphynx',
+        color: 'Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'neon.jpg',
+        name: 'Tetra Neon',
+        color: 'Vermelho e Azul',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'matogrosso.jpg',
+        name: 'Mato Grosso',
+        color: 'Laranja',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'limpavidro.jpg',
+        name: 'Limpa Vidro',
+        color: 'Verde e Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'tanictis.jpg',
+        name: 'Tanictis',
+        color: 'Vermelho',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'acara.jpg',
+        name: 'Acará Bandeira',
+        color: 'Preto',
+        sex: 'Masculino'
+    },
+]
+
+//Cria um objeto onde vai conter todas as funções para fazer a manipulação dos pet na pagina
+export const Pet = {
+    // Função para mostrar todas as funções.
+    getAll: (): Pet[] => {
+        return data;
+    },
+
+    // Função para filtrar pelo tipo do pet, onde sera criado um novo array com a função filter onde ele retornara true se o type do item do array for o mesmo type que voce passou pelo parametro.
+    getFromType: (type: PetType): Pet[] => {
+        return data.filter(item => item.type === type);
+    },
+
+    // Filtra o pet pelo nome , se ele pesquisar por algum nome e ele retornar maior que -1 então quer dizer que ele achou . Tem que colocar para lowerCase pois se escrever em maiusculo não vai achar pois é caseSensitive.
+    getFromName: (name: string): Pet[] => {
+        return data.filter(item => 
+            item.name.toLocaleLowerCase().indexOf(name.toLowerCase()) > -1    
+        )
+    }
+
+}
